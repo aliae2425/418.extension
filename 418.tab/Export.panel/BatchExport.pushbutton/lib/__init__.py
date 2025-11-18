70cb33667e31145a40d7799f00b0a7772f32d09b
aliae <noam.carmi80@gmail.com>
2025-11-18 22:27:44 +0100

    premare hard merge

--- 418.tab/Export.panel/BatchExport.pushbutton/lib/__init__.py
+++
@@ -1,3 +0,0 @@
-# Make 'lib' a package so imports like 'from lib.GUI import GUI' work in all Python runtimes (incl. IronPython/pyRevit).
-
-__all__ = ["GUI", "config", "sheets", "piker", "naming", "destination", "dwg_export", "pdf_export", "setup_editor", "exporter"]
