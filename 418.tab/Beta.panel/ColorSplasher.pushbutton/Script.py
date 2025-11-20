b8163fadd7fe29c8825bb14424c46c9a4a828a03
aliae <noam.carmi80@gmail.com>
2025-11-20 20:23:01 +0100

    mise en place des futures fonctions a coder

---
+++ 418.tab/Beta.panel/ColorSplasher.pushbutton/Script.py
@@ -0,0 +1,20 @@
+__title__ = "Color Splasher 2.0"
+__doc__ = """
+    Version: -
+    Auteur : Aliae
+    _____________________________________________
+
+    Cette fonctionnalité sera bientôt disponible
+    Nouvelle version de color splasher avec plus d'options
+
+    Todo : fork color splasher existant et ajouter options
+    - choix predefinis de couleurs
+    - rework UI
+    - sauvegarde des parametres entre sessions
+    _____________________________________________
+"""
+__author__ = 'Aliae'                               
+__min_revit_ver__ = 2026       
+
+if __name__ == "__main__":
+    print("Cette fonctionnalité sera bientôt disponible")
---
+++ 418.tab/layout.panel/Reperage.pushbutton/script.py
@@ -0,0 +1,16 @@
+__title__ = "Reperage"
+__doc__ = """
+    Version: -
+    Auteur : Aliae
+    _____________________________________________
+
+    Cette fonctionnalité sera bientôt disponible
+    crée un repérages des coupes et élévations sur les feuilles selectionnées
+
+    _____________________________________________
+"""
+__author__ = 'Aliae'                               
+__min_revit_ver__ = 2026      
+
+if __name__ == "__main__":
+    print("Cette fonctionnalité sera bientôt disponible")
