01c6379fb4c7012651c1e01a3ba791906a465ff6
aliae <noam.carmi80@gmail.com>
2025-12-09 21:06:01 +0100

    Add Update button script and associated icons

---
+++ 418.tab/418.panel/Update.pushbutton/script.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------- info pyrevit ------------------------------- #
+__title__ = "Exportation"
+__doc__ = """
+    Version 0.4
+    Auteur : Aliae
+    _____________________________________________
+
+    Export les feuilles par jeu de feuilles en fonction des parametre du jeu 
+
+    Export : inclus les feuilles du jeux a l'export 
+    Export par feuilles : exporte les feuilles une par une, par defaut export en carnet compilé avec le nom du jeu
+    Export en DWG : Export en DWG en plus du PDF
+    _____________________________________________
+"""
+__author__ = 'Aliae'                               
+__min_revit_ver__ = 2026          
