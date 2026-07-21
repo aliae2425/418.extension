39f3278774bcbece4be454ac73496559d67802d2
aliae2425 <noam.carmi80@gmail.com>
2026-07-21 16:04:50 +0200

    feat(ManageFiltre): scaffold MVVM complet

---
+++ 418.tab/Manage.panel/ManageFiltre.pushbutton/script.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+__title__ = "Filtres"
+__doc__ = "Gestion avancée des filtres de vue."
+__author__ = 'Aliae'
+__min_revit_ver__ = 2026
+
+try:
+    doc = __revit__.ActiveUIDocument.Document  # type: ignore
+except Exception:
+    doc = None
+
+from lib.viewmodels.MainViewModel import MainViewModel
+from lib.views.MainWindowView import MainWindowView
+
+if __name__ == '__main__':
+    vm = MainViewModel(doc=doc)
+    view = MainWindowView(vm)
+    view.show()
