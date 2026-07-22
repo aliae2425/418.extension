deed1c7989fd57df44d4df7e2fba95249a8c3092
aliae2425 <noam.carmi80@gmail.com>
2026-07-22 09:58:08 +0200

    feat(ImageCrop): enhance image cropping functionality to support multiple crop regions and improve documentation

--- 418.tab/Tools.panel/ImageCrop.pushbutton/script.py
+++ 418.tab/Tools.panel/ImageCrop.pushbutton/script.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Recadre une image importee selon une region remplie ou une ligne de detail.
+Recadre une image importee selon une ou plusieurs zones de pochage.
 
-Selectionner une image + une region remplie (ou une ligne de detail) qui
-delimite la zone a conserver, puis lancer la commande. L'image recadree
-remplace l'image d'origine a la meme position.
+Selectionner UNE image + une ou plusieurs regions remplies (ou lignes de
+detail) qui delimitent les zones a conserver, puis lancer la commande.
+Chaque zone produit un morceau d'image cale exactement dans son cadre.
+L'image d'origine est conservee. Les zones de pochage sont conservees et,
+une fois le crop resolu, affichees sans fond avec un cadre vert (override
+graphique propre a la vue active, non destructif).
 """
 from __future__ import unicode_literals, division
 #pylint: disable=E0401,W0621,W0631,C0413,C0111,C0103
-__doc__ = 'Recadre une image selon une region remplie ou une ligne de detail.'
+__doc__ = 'Recadre une image selon une ou plusieurs zones de pochage.'
 __author__ = 'Aliae'
 
 import sys
@@ -67,6 +70,7 @@ def create_img_copy(img_path):
 
 
 def crop_image(img_path, rectangle_crop):
+    """ Ecrit un fichier recadre (copie du source) selon rectangle_crop. """
     if not os.path.exists(img_path):
         raise Exception('Image source introuvable : {}'.format(img_path))
     source_bmp = Bitmap(img_path)
@@ -82,187 +86,217 @@ def crop_image(img_path, rectangle_crop):
     return new_img_path
 
 
-img_element, img_type, element_bbox, crop_element = None, None, None, None
-img_path = None
+def read_image_info(img_element, img_type):
+    """ Lit une fois tous les parametres de l'image source (partages entre
+    toutes les zones de pochage). Retourne un dict. """
+    bip_filename = DB.BuiltInParameter.RASTER_SYMBOL_FILENAME
+    bip_width_px = DB.BuiltInParameter.RASTER_SYMBOL_PIXELWIDTH
+    bip_height_px = DB.BuiltInParameter.RASTER_SYMBOL_PIXELHEIGHT
+    bip_resolution = DB.BuiltInParameter.RASTER_SYMBOL_RESOLUTION
+    bip_width_ft = DB.BuiltInParameter.RASTER_SHEETWIDTH
+    bip_height_ft = DB.BuiltInParameter.RASTER_SHEETHEIGHT
+
+    info = {
+        'path': img_type.get_Parameter(bip_filename).AsString(),
+        'width_px': img_type.get_Parameter(bip_width_px).AsInteger(),
+        'height_px': img_type.get_Parameter(bip_height_px).AsInteger(),
+        'resolution': img_type.get_Parameter(bip_resolution).AsInteger(),
+        'width': img_element.get_Parameter(bip_width_ft).AsDouble(),
+        'height': img_element.get_Parameter(bip_height_ft).AsDouble(),
+        'bbox': img_element.get_BoundingBox(doc.ActiveView),
+        'bip_width_ft': bip_width_ft,
+    }
+
+    # Resolution DPI : AsInteger() peut renvoyer 0 selon le stockage du
+    # parametre. On la recalcule depuis pixels / taille physique
+    # (DPI = pixels / pouces ; width est en pieds -> * 12 pouces).
+    if not info['resolution'] or info['resolution'] <= 0:
+        inches_w = info['width'] * 12.0
+        info['resolution'] = (int(round(info['width_px'] / inches_w))
+                              if inches_w else 0)
+        print('[CropImage] Resolution recalculee = {} DPI'.format(
+            info['resolution']))
+    return info
+
+
+def crop_and_place(img_info, region):
+    """ Recadre l'image source selon la bbox de 'region' et place le morceau
+    dans son cadre. Retourne l'ImageInstance creee, ou None si la region est
+    invalide (sans bbox, hors image, trop fine...). """
+    view = doc.ActiveView
+    region_bbox = region.get_BoundingBox(view)
+    if not region_bbox:
+        print('[CropImage] Region {} sans bounding box, ignoree.'.format(
+            region.Id))
+        return None
+
+    # Dimensions absolues du cadre.
+    cropbox_height_ft = region_bbox.Max.Y - region_bbox.Min.Y
+    cropbox_width_ft = region_bbox.Max.X - region_bbox.Min.X
+
+    # Origine relative de la decoupe par rapport au coin de l'image.
+    lw_left_crop_pt = region_bbox.Min - img_info['bbox'].Min
+    up_left_crop_pt = lw_left_crop_pt + DB.XYZ(0, cropbox_height_ft, 0)
+    crop_pt_x_ft = up_left_crop_pt.X
+    crop_pt_y_ft = img_info['height'] - up_left_crop_pt.Y
+
+    # Conversion pieds -> pixels.
+    x_scale = img_info['width_px'] / img_info['width']
+    y_scale = img_info['height_px'] / img_info['height']
+    rectangle_crop = Rectangle(int(round(crop_pt_x_ft * x_scale)),
+                               int(round(crop_pt_y_ft * y_scale)),
+                               int(round(cropbox_width_ft * x_scale)),
+                               int(round(cropbox_height_ft * y_scale)))
+    print('[CropImage] Region {} -> rectangle px x={} y={} w={} h={}'.format(
+        region.Id, rectangle_crop.X, rectangle_crop.Y,
+        rectangle_crop.Width, rectangle_crop.Height))
+
+    if rectangle_crop.Width <= 0 or rectangle_crop.Height <= 0:
+        print('[CropImage] Region {} : zone vide/hors image, ignoree.'.format(
+            region.Id))
+        return None
+
+    # Fichier recadre sur disque.
+    new_img_path = crop_image(img_info['path'], rectangle_crop)
+    print('[CropImage] Fichier recadre : {}'.format(new_img_path))
+
+    # Type d'image (False = chemin absolu).
+    type_options = DB.ImageTypeOptions(
+        new_img_path, False, DB.ImageTypeSource.Import)
+    if img_info['resolution'] and img_info['resolution'] > 0:
+        type_options.Resolution = img_info['resolution']
+    new_img_type = DB.ImageType.Create(doc, type_options)
+
+    # Regenere avant de placer l'instance (sinon "internal error").
+    doc.Regenerate()
+
+    # Instance centree exactement sur le centre du cadre.
+    placement = DB.ImagePlacementOptions(
+        get_bbox_center_pt(region_bbox), DB.BoxPlacement.Center)
+    new_instance = DB.ImageInstance.Create(
+        doc, view, new_img_type.Id, placement)
+
+    # Cale la largeur sur le cadre (la hauteur suit le ratio, identique).
+    width_param = new_instance.get_Parameter(img_info['bip_width_ft'])
+    if width_param and not width_param.IsReadOnly:
+        width_param.Set(cropbox_width_ft)
+
+    print('[CropImage] Region {} : morceau place (id {}).'.format(
+        region.Id, new_instance.Id))
+    return new_instance
+
+
+# Couleur du cadre applique aux zones resolues (vert). Modifier ici au besoin.
+GREEN = DB.Color(0, 176, 80)
+
+
+def mark_region_solved(region):
+    """ Marque une zone de pochage comme "resolue" dans la vue active :
+    aucun remplissage (patterns masques) + contour vert. Override propre a
+    la vue, non destructif (l'element et son type ne sont pas modifies). """
+    ogs = DB.OverrideGraphicSettings()
+    # Sans fond : masque les motifs de surface et de coupe (avant + arriere).
+    ogs.SetSurfaceForegroundPatternVisible(False)
+    ogs.SetSurfaceBackgroundPatternVisible(False)
+    ogs.SetCutForegroundPatternVisible(False)
+    ogs.SetCutBackgroundPatternVisible(False)
+    # Cadre vert.
+    ogs.SetProjectionLineColor(GREEN)
+    ogs.SetCutLineColor(GREEN)
+    ogs.SetProjectionLineWeight(4)
+    doc.ActiveView.SetElementOverrides(region.Id, ogs)
+
+
+# --------------------------------------------------------------------------
+# Selection : 1 image source + N zones de pochage.
+# --------------------------------------------------------------------------
+img_element, img_type = None, None
+crop_elements = []
+
 elements = get_selected_elements()
 print('=' * 50)
 print('[CropImage] {} element(s) selectionne(s)'.format(len(elements)))
 for element in elements:
-    # Region de decoupe : region remplie ou ligne de detail.
+    # Zone de pochage : region remplie ou ligne de detail.
     if isinstance(element, (DB.FilledRegion, DB.DetailLine)):
-        crop_element = element
-        element_bbox = element.get_BoundingBox(doc.ActiveView)
-        print('[CropImage] Cadre de decoupe : {} (id {})'.format(
+        crop_elements.append(element)
+        print('[CropImage] Zone de pochage : {} (id {})'.format(
             type(element).__name__, element.Id))
         continue
 
+    # Sinon, cherche une image parmi les types valides.
     for valid_type_id in element.GetValidTypes():
         valid_type = doc.GetElement(valid_type_id)
-
         if isinstance(valid_type, DB.ImageType):
+            if img_element is not None:
+                print('[CropImage] Plusieurs images selectionnees : '
+                      'seule la derniere est utilisee.')
             img_element = element
             img_type = valid_type
-
-            # Definitions BIP du type
-            bip_filename = DB.BuiltInParameter.RASTER_SYMBOL_FILENAME
-            bip_height_px = DB.BuiltInParameter.RASTER_SYMBOL_PIXELHEIGHT
-            bip_width_px = DB.BuiltInParameter.RASTER_SYMBOL_PIXELWIDTH
-            bip_resolution = DB.BuiltInParameter.RASTER_SYMBOL_RESOLUTION
-
-            # Parametres du type
-            img_path = img_type.get_Parameter(bip_filename).AsString()
-            img_width_px = img_type.get_Parameter(bip_width_px).AsInteger()
-            img_height_px = img_type.get_Parameter(bip_height_px).AsInteger()
-            img_resolution = img_type.get_Parameter(bip_resolution).AsInteger()
-
-            # Definitions BIP de l'instance
-            bip_scale = DB.BuiltInParameter.RASTER_VERTICAL_SCALE
-            bip_width_ft = DB.BuiltInParameter.RASTER_SHEETWIDTH   # Largeur
-            bip_height_ft = DB.BuiltInParameter.RASTER_SHEETHEIGHT  # Hauteur
-
-            # Parametres de l'instance
-            img_scale = img_element.get_Parameter(bip_scale).AsDouble()
-            img_width = img_element.get_Parameter(bip_width_ft).AsDouble()
-            img_height = img_element.get_Parameter(bip_height_ft).AsDouble()
-            img_bbox = img_element.get_BoundingBox(doc.ActiveView)
-
-            print('[CropImage] Image trouvee (id {})'.format(img_element.Id))
-            print('[CropImage]   chemin     = {}'.format(img_path))
-            print('[CropImage]   pixels     = {} x {} px'.format(
-                img_width_px, img_height_px))
-            print('[CropImage]   taille     = {} x {} ft'.format(
-                img_width, img_height))
-            print('[CropImage]   resolution = {} (param brut)'.format(
-                img_resolution))
+            print('[CropImage] Image source (id {})'.format(img_element.Id))
             break
 
-if not img_element or not crop_element or not element_bbox:
+
+# --------------------------------------------------------------------------
+# Validation puis traitement.
+# --------------------------------------------------------------------------
+if not img_element or not crop_elements:
     rpw.ui.forms.Alert(
-        'Selectionner une image + une region remplie ou une ligne de detail.'
+        'Selectionner UNE image + au moins une zone de pochage '
+        '(region remplie ou ligne de detail).'
     )
-elif not img_path:
-    # Cas des images integrees au modele (pas de fichier externe sur disque).
-    rpw.ui.forms.Alert(
-        "Impossible de recuperer le chemin de l'image "
-        "(elle est peut-etre integree au modele et non liee a un fichier)."
-    )
-elif not img_width or not img_height or not img_width_px or not img_height_px:
-    rpw.ui.forms.Alert("Dimensions de l'image invalides (largeur/hauteur nulle).")
 else:
-    print('[CropImage] Image + cadre OK, calcul du recadrage...')
+    img_info = read_image_info(img_element, img_type)
+    print('[CropImage] chemin     = {}'.format(img_info['path']))
+    print('[CropImage] pixels     = {} x {} px'.format(
+        img_info['width_px'], img_info['height_px']))
+    print('[CropImage] taille     = {} x {} ft'.format(
+        img_info['width'], img_info['height']))
+    print('[CropImage] zones a decouper = {}'.format(len(crop_elements)))
 
-    # Resolution DPI : AsInteger() peut renvoyer 0 selon le stockage du
-    # parametre. On la recalcule alors depuis pixels / taille physique
-    # (DPI = pixels / pouces ; img_width est en pieds -> * 12 pouces).
-    if not img_resolution or img_resolution <= 0:
-        inches_w = img_width * 12.0
-        img_resolution = int(round(img_width_px / inches_w)) if inches_w else 0
-        print('[CropImage] Resolution recalculee = {} DPI'.format(img_resolution))
-
-    # Hauteur/largeur absolues de la boite de decoupe.
-    cropbox_height_ft = element_bbox.Max.Y - element_bbox.Min.Y
-    cropbox_width_ft = element_bbox.Max.X - element_bbox.Min.X
-
-    # Coordonnee relative de la boite de decoupe / coin de l'image.
-    lw_left_crop_pt = element_bbox.Min - img_bbox.Min
-    up_left_crop_pt = lw_left_crop_pt + DB.XYZ(0, cropbox_height_ft, 0)
-
-    # Origine relative pour le recadrage.
-    crop_pt_x_ft = up_left_crop_pt.X
-    crop_pt_y_ft = img_height - up_left_crop_pt.Y
-
-    # Facteur de conversion pieds -> pixels.
-    x_ft_to_px_scale = img_width_px / img_width
-    y_ft_to_px_scale = img_height_px / img_height
-
-    # Conversion de l'espace en pieds vers l'espace en pixels.
-    crop_pt_x_px = crop_pt_x_ft * x_ft_to_px_scale
-    crop_pt_y_px = crop_pt_y_ft * y_ft_to_px_scale
-    cropbox_width_px = cropbox_width_ft * x_ft_to_px_scale
-    cropbox_height_px = cropbox_height_ft * y_ft_to_px_scale
-
-    # System.Drawing.Rectangle attend des entiers.
-    rectangle_crop = Rectangle(int(round(crop_pt_x_px)),
-                               int(round(crop_pt_y_px)),
-                               int(round(cropbox_width_px)),
-                               int(round(cropbox_height_px)))
-
-    print('[CropImage] Cadre : {} x {} ft'.format(
-        cropbox_width_ft, cropbox_height_ft))
-    print('[CropImage] Rectangle de decoupe (px) : x={} y={} w={} h={}'.format(
-        rectangle_crop.X, rectangle_crop.Y,
-        rectangle_crop.Width, rectangle_crop.Height))
-
-    if rectangle_crop.Width <= 0 or rectangle_crop.Height <= 0:
+    if not img_info['path']:
+        # Image integree au modele (pas de fichier externe sur disque).
         rpw.ui.forms.Alert(
-            "La zone de decoupe est vide ou hors de l'image "
-            "(region trop fine ou en dehors de l'image)."
+            "Impossible de recuperer le chemin de l'image "
+            "(elle est peut-etre integree au modele et non liee a un fichier)."
         )
-        sys.exit(0)
-
-    new_img_path = crop_image(img_path, rectangle_crop)
-    print('[CropImage] Fichier recadre cree : {}'.format(new_img_path))
-
-    # Point de placement = centre du cadre forme par la region / les lignes.
-    placement_pt = get_bbox_center_pt(element_bbox)
-    print('[CropImage] Point de placement (centre cadre) : {}, {}, {}'.format(
-        placement_pt.X, placement_pt.Y, placement_pt.Z))
-
-    # Cree la nouvelle image dans Revit et la cale dans le cadre.
-    # Transaction explicite : rpw.db.Transaction pouvait avaler l'exception
-    # (rollback silencieux -> fichier cree mais rien dans Revit).
-    t = DB.Transaction(doc, 'Crop Image')
-    t.Start()
-    try:
-        # 1. Type d'image a partir du fichier recadre.
-        #    False = chemin absolu (et non relatif au projet).
-        type_options = DB.ImageTypeOptions(
-            new_img_path, False, DB.ImageTypeSource.Import)
-        # Resolution : uniquement si strictement positive (sinon Revit rejette).
-        if img_resolution and img_resolution > 0:
-            type_options.Resolution = img_resolution
-        else:
-            print('[CropImage] Resolution non definie, valeur par defaut utilisee.')
-        new_img_type = DB.ImageType.Create(doc, type_options)
-        print('[CropImage] ImageType cree (id {})'.format(new_img_type.Id))
-
-        # Force la mise a jour du document avant de placer l'instance,
-        # sinon ImageInstance.Create peut lever une "internal error".
-        doc.Regenerate()
-
-        # 2. Instance centree exactement sur le centre du cadre.
-        placement = DB.ImagePlacementOptions(
-            placement_pt, DB.BoxPlacement.Center)
-        new_img_instance = DB.ImageInstance.Create(
-            doc, doc.ActiveView, new_img_type.Id, placement)
-        print('[CropImage] ImageInstance placee (id {})'.format(
-            new_img_instance.Id))
-
-        # 3. Ajuste la largeur pour caler l'image exactement dans le cadre
-        #    (la hauteur suit le ratio, identique a celui du cadre).
-        new_img_width = new_img_instance.get_Parameter(bip_width_ft)
-        if new_img_width and not new_img_width.IsReadOnly:
-            new_img_width.Set(cropbox_width_ft)
-            print('[CropImage] Largeur calee sur le cadre : {} ft'.format(
-                cropbox_width_ft))
-        else:
-            print('[CropImage] Parametre largeur introuvable ou en lecture seule.')
-
-        # 4. Supprime l'image d'origine et le cadre de decoupe.
-        doc.Delete(img_element.Id)
-        doc.Delete(crop_element.Id)
-        print('[CropImage] Image d\'origine et cadre supprimes.')
-
-        t.Commit()
-        print('[CropImage] Termine avec succes.')
-    except Exception:
-        t.RollBack()
-        import traceback
-        print('[CropImage] ECHEC :')
-        print(traceback.format_exc())
-        UI.TaskDialog.Show(
-            'CropImage - Erreur',
-            "Echec de la creation de l'image dans Revit :\n\n{}".format(
-                traceback.format_exc())
+    elif (not img_info['width'] or not img_info['height']
+          or not img_info['width_px'] or not img_info['height_px']):
+        rpw.ui.forms.Alert(
+            "Dimensions de l'image invalides (largeur/hauteur nulle)."
         )
-        raise
+    else:
+        # Une seule transaction pour toutes les zones.
+        t = DB.Transaction(doc, 'Crop Image')
+        t.Start()
+        try:
+            placed = 0
+            for region in crop_elements:
+                instance = crop_and_place(img_info, region)
+                if instance is not None:
+                    # Zone conservee, stylisee "resolue" : sans fond + cadre vert.
+                    mark_region_solved(region)
+                    print('[CropImage] Zone {} stylisee (resolue).'.format(
+                        region.Id))
+                    placed += 1
+            # L'image d'origine est CONSERVEE (pas de suppression).
+
+            t.Commit()
+            print('[CropImage] Termine : {} morceau(x) cree(s) sur {} zone(s).'
+                  .format(placed, len(crop_elements)))
+            if placed == 0:
+                rpw.ui.forms.Alert(
+                    "Aucune zone valide : verifier que les zones de pochage "
+                    "sont bien situees sur l'image."
+                )
+        except Exception:
+            t.RollBack()
+            import traceback
+            print('[CropImage] ECHEC :')
+            print(traceback.format_exc())
+            UI.TaskDialog.Show(
+                'CropImage - Erreur',
+                "Echec de la creation des images dans Revit :\n\n{}".format(
+                    traceback.format_exc())
+            )
+            raise
